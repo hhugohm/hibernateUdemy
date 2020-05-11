33651048c80f8c1de7f23f32a09254616a9fbdba
hhugohm <hhugohm@gmail.com>
2020-05-11 15:33:19 -0500

    configuration xml

--- src/main/java/com/infiniteskills/data/HibernateUtil.java
+++ src/main/java/com/infiniteskills/data/HibernateUtil.java
@@ -1,28 +1,47 @@
 package com.infiniteskills.data;
 
 import org.hibernate.SessionFactory;
+import org.hibernate.boot.Metadata;
+import org.hibernate.boot.MetadataSources;
+import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
-import org.hibernate.cfg.Configuration;
 
 
 
 public class HibernateUtil {
+	
+	private static StandardServiceRegistry registry;
+	private static SessionFactory sessionFactory =buildSessionFactory();
 
-	private static final SessionFactory sessionFactory = buildSessionFactory();
+	//private static final SessionFactory sessionFactory = buildSessionFactory();
 
 	private static SessionFactory buildSessionFactory() {
-		try {
-			Configuration configuration = new Configuration();
-			return configuration
-					.buildSessionFactory(new StandardServiceRegistryBuilder().applySettings(configuration.getProperties())
-							.build());
-		} catch (Exception e) {
-			e.printStackTrace();
-			throw new RuntimeException("There was an error building the factory");
-		}
+		if (sessionFactory == null) {
+            try {
+                // Create registry
+                registry = new StandardServiceRegistryBuilder().configure().build();
+                // Create MetadataSources
+                MetadataSources sources = new MetadataSources(registry);
+                // Create Metadata
+                Metadata metadata = sources.getMetadataBuilder().build();
+                // Create SessionFactory
+                sessionFactory = metadata.getSessionFactoryBuilder().build();
+            } catch (Exception e) {
+                e.printStackTrace();
+                if (registry != null) {
+                    StandardServiceRegistryBuilder.destroy(registry);
+                }
+            }
+        }
+        return sessionFactory;
 	}
 	
 	public static SessionFactory getSessionFactory(){
 		return sessionFactory;
 	}
+	public static void shutdown() {
+        if (registry != null) {
+            StandardServiceRegistryBuilder.destroy(registry);
+        }
+    }
 }
